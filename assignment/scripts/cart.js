f11c6fd0814f7e1b0116727fe953cac1653fd4da
Cali Knowlden <calbearnc@gmail.com>
2023-11-15 20:16:36 -0600

    answered all required questions and first 3 Stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,14 +1,46 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+addItem('marbles');
+addItem(`potato`);
+addItem('peanut');
+addItem('pear');
+addItem('cherry');
 
+//console.log(addItem('apple'));
+console.log(`The basket contains: ${basket}`);
 
+function listItems(){
+    for (let item of basket){
+    console.log(item)
+    };
+};
 
+listItems();
 
+function empty(){
+    basket.length = 0
+}
 
+empty();
+console.log(`The basket now has ${basket.length} items`);
 
+let maxItems = 5;
 
+function isFull(){
+    if (basket.length < maxItems){
+    return false;
+    } else{
+    return true;
+    }
+};
 
+console.log(`The basket is full? ${isFull()}`);
 
 
 
